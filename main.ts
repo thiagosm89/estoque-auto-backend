44e9d76864669604f9a42b58e51da5d8a2a6e91d
thiagosm89 <thiagopereiramello@gmail.com>
2025-06-28 20:19:24 -0300

    wip: adicionando a edge de registro de company

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 import { execute as executeRegisterCompany } from "./supabase/functions/register-company/index.ts";
 import { execute as executeOnboarding } from "./supabase/functions/onboarding-company/index.ts";
+import { execute as executeGetCurrentTerm } from "./supabase/functions/get-current-term/index.ts";
 
 console.log("[dev] API local rodando em http://localhost:8787");
 
@@ -13,6 +14,10 @@ serve(async (req: Request, ctx) => {
     if (url.pathname === "/functions/v1/onboarding-company" && req.method === "POST") {
         // Chama o handler da nova edge function de onboarding
         return executeOnboarding()(req, ctx);
+    }
+    if (url.pathname === "/functions/v1/get-current-term" && req.method === "POST") {
+        // Chama o handler da nova edge function de onboarding
+        return executeGetCurrentTerm()(req, ctx);
     }
     return new Response("Not Found", { status: 404 });
 }, { port: 8787 });
